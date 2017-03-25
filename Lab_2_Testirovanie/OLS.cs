438e189d42aa178cfa2e823ae7f7c109bcf9adbb
Julya <julyaanashkin@yandex.ru>
2017-03-25 22:22:28 +0300

    Пишем код для тестов Insert_Node и Insert_Two_Node.

--- Lab_2_Testirovanie/OLS.cs
+++ Lab_2_Testirovanie/OLS.cs
@@ -21,5 +21,28 @@ namespace Lab_2_Testirovanie
         public Node<T> head; // голова
         public Node<T> tail; // хвост
         public int count;  // количество элементов в списке
+
+        // добвление в начало
+        public void Add_in_Head(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            node.Next = head;
+            head = node;
+            if (count == 0)
+                tail = head;
+            count++;
+        }
+        // добавление  в конец
+        public void Add_in_Tail(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (head == null)
+                head = node;
+            else
+                tail.Next = node;
+            tail = node;
+            count++;
+        }
+        
     }
 }
