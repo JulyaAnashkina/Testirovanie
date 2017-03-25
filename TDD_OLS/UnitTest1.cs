2387e152432c10a9155ca5b6caba89d433ce53a7
Julya <julyaanashkin@yandex.ru>
2017-03-25 21:08:19 +0300

    Test Create_OLS.

---
+++ TDD_OLS/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Lab_2_Testirovanie;
+
+namespace TDD_OLS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Create_OLS()
+        {
+            OLS<string> list = new OLS<string>();
+
+            Assert.IsNotNull(list);
+        }
+    }
+}
