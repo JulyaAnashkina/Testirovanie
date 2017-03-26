39d4efb1ae7767b3da77437a7d68b39aef5ef4b2
Julya <julyaanashkin@yandex.ru>
2017-03-26 22:51:07 +0300

    Тесты УдалитьУзелИлиСписок

--- BDD_OLS/УдалитьУзелИлиСписокSteps.cs
+++ BDD_OLS/УдалитьУзелИлиСписокSteps.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Lab_2_Testirovanie;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using TechTalk.SpecFlow;
 
 namespace BDD_OLS
@@ -6,40 +8,57 @@ namespace BDD_OLS
     [Binding]
     public class УдалитьУзелИлиСписокSteps
     {
+        private OLS<string> list;
+
         [Given(@"инициализирую список")]
         public void ДопустимИнициализируюСписок()
         {
-            ScenarioContext.Current.Pending();
+            list = new OLS<string>();
         }
-        
+
         [Given(@"я добавляю в него элементы")]
         public void ДопустимЯДобавляюВНегоЭлементы()
         {
-            ScenarioContext.Current.Pending();
+            list.Add_in_Tail("Смирнов");
+            list.Add_in_Head("Петров");
+            list.Add_in_Head("Иванов");
+            list.Add_in_Head("Васичкин");
         }
-        
+
         [When(@"удаляю заданный узел")]
         public void ЕслиУдаляюЗаданныйУзел()
         {
-            ScenarioContext.Current.Pending();
+            list.Remove("Петров");
         }
-        
+
         [When(@"удаляю список")]
         public void ЕслиУдаляюСписок()
         {
-            ScenarioContext.Current.Pending();
+            list.Clear();
         }
-        
+
         [Then(@"данный узел удален")]
         public void ТоДанныйУзелУдален()
         {
-            ScenarioContext.Current.Pending();
+            Assert.AreEqual(list.count, 3);
+
+            OLS<string>.Node<string> current = list.head;
+            while (current != null)
+            {
+                Assert.AreNotEqual(current.Data, "Петров");
+                current = current.Next;
+            }
         }
-        
+
         [Then(@"список удален")]
         public void ТоСписокУдален()
         {
-            ScenarioContext.Current.Pending();
+            OLS<string>.Node<string> current = list.head;
+            while (current != null)
+            {
+                Assert.AreEqual(current.Data, null);
+                current = current.Next;
+            }
         }
     }
 }
